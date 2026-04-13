7973c1b636ecbaf4096e779b1ebdbd84eaaaf7ad
unknown <priyanshupandey9931@gmail.com>
2026-04-14 00:02:21 +0530

    UC3

--- App/src/HelloWorld.java
+++ App/src/HelloWorld.java
@@ -1,9 +1,14 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+class HelloApp {
+    public void display(String[] args) {
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello User");
+            System.out.println("Hello World");
         }
+    }
+
+    public static void main(String[] args) {
+        HelloApp obj = new HelloApp();
+        obj.display(args);
     }
 }
